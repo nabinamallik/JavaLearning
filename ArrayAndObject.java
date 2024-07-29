0ac9a44b2c5e0341b401e3c8ac07512b00efdd18
nabinamallik <nabinamallik2003@gmail.com>
2024-07-29 10:37:23 +0530

    intial commit

---
+++ ArrayAndObject.java
@@ -0,0 +1,7 @@
+public class ArrayAndObject {
+    public static void main(String[] args) {
+        int num[] = new int[4]; // Drawback : you cant extend the array , you only can onlly put same type of
+                                // data as per datatype.
+        System.out.println(num);
+    }
+}
