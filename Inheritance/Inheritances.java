a3e3288d6365cb6c86bd805e3295b58969091f4a
nabinamallik <nabinamallik2003@gmail.com>
2024-08-03 18:04:54 +0530

    java

---
+++ Inheritance/Inheritances.java
@@ -0,0 +1,12 @@
+
+public class Inheritances {
+    public static void main(String[] args) {
+        AdvCalc obj = new AdvCalc();
+
+        System.out.println(obj.add(2, 4));
+        System.out.println(obj.sub(4, 2));
+        System.out.println(obj.mul(3, 5));
+        System.out.println(obj.div(9, 3));
+
+    }
+}
